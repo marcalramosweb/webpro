0d81f713564ea3fcf1b58fde67dea3a44f867eef
Marcal Ramos <marcalramos@gmail.com>
2015-08-27 17:46:44 +0000

    added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ActionMailer::Base
+    
+    defaut to: 'marcalramos@gmail.com'
+    
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Ruby_Test Contact form Message')
+    end
+end
