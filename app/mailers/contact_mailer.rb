b3be3e2c0d16e1256ae84652619659f6bb1b63c9
Marcal Ramos <marcalramos@gmail.com>
2015-08-27 20:59:23 +0000

    change on cantact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ActionMailer::Base
     
-    defaut to: 'marcalramos@gmail.com'
+    default to: 'marcalramos@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
